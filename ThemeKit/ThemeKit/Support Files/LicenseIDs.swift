b87de7a045dbb3d39258985b71dd0147093cb923
Josh Campion <joshcampion89@gmail.com>
2016-04-04 07:47:21 +0100

    Updated License IDs for project PSEO

--- ThemeKit/ThemeKit/Support Files/LicenseIDs.swift
+++ ThemeKit/ThemeKit/Support Files/LicenseIDs.swift
@@ -1,7 +1,8 @@
 let licensedIdentifiers = [
 	"uk.co.joshcampion.ThemeKitDemo",
 	"com.apple.InterfaceBuilder.IBCocoaTouchPlugin.IBCocoaTouchTool",
-	"uk.co.thedistance.My-Hiscox",
-	"uk.co.thedistance.My-HiscoxTest",
-	"uk.co.thedistance.My-HiscoxTests",
+	"io.pocketseo",
+	"io.pocketseo.URLMetricsExtension",
+	"io.pocketseoTests",
+	"io.pocketseoUITests",
 ]
